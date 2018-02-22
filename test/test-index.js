a557c0540d43804e0c48b6e82eec6e09a0daf90d
sethvincent <sethvincent@gmail.com>
2018-02-22 17:57:53 -0500

    test errors from rejected promises

--- test/test-index.js
+++ test/test-index.js
@@ -84,3 +84,42 @@ test.cb('A non-WorkflowError is raised', (t) => {
 
   return cumulusMessageAdapter.runCumulusTask(businessLogic, {}, {}, callback);
 });
+
+test.cb('A promise returns an error', (t) => {
+  function businessLogic() {
+    return Promise.reject(new Error('oh no'));
+  }
+
+  function callback(err, data) {
+    t.is(err.name, 'Error');
+    t.is(err.message, 'oh no');
+    t.is(data, undefined);
+    t.end();
+  }
+
+  return cumulusMessageAdapter.runCumulusTask(businessLogic, {}, {}, callback);
+});
+
+test.cb('A Promise WorkflowError is returned properly', (t) => {
+  const inputEvent = { a: 1 };
+
+  const expectedOutput = {
+    a: 1,
+    payload: null,
+    exception: 'SomeWorkflowError'
+  };
+
+  function businessLogic() {
+    const error = new Error('Oh no');
+    error.name = 'SomeWorkflowError';
+    return Promise.reject(error);
+  }
+
+  function callback(err, data) {
+    t.is(err, null);
+    t.deepEqual(data, expectedOutput);
+    t.end();
+  }
+
+  return cumulusMessageAdapter.runCumulusTask(businessLogic, inputEvent, {}, callback);
+});
