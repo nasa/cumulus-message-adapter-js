655f825de4958e6cc66ea402486b5b03f4265afd
Mark Boyd <markdavidboyd@gmail.com>
2019-12-03 15:40:30 -0500

    update release script to point to correct repo

--- bin/release.sh
+++ bin/release.sh
@@ -5,7 +5,7 @@ export VERSION_NUMBER=$(jq --raw-output .version package.json)
 export VERSION_TAG="v$VERSION_NUMBER"
 export LATEST_TAG=$(curl -H \
   "Authorization: token $GITHUB_TOKEN" \
-  https://api.github.com/repos/markdboyd/cumulus-message-adapter-js/tags | jq --raw-output '.[0].name')
+  https://api.github.com/repos/nasa/cumulus-message-adapter-js/tags | jq --raw-output '.[0].name')
 
 if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
   echo "tag does not exist for version $VERSION_TAG, creating tag"
@@ -17,7 +17,7 @@ fi
 
 export RELEASE_URL=$(curl -H \
   "Authorization: token $GITHUB_TOKEN" \
-  https://api.github.com/repos/markdboyd/cumulus-message-adapter-js/releases/tags/$VERSION_TAG | jq --raw-output '.url // ""')
+  https://api.github.com/repos/nasa/cumulus-message-adapter-js/releases/tags/$VERSION_TAG | jq --raw-output '.url // ""')
 
 if [ -z "$RELEASE_URL" ]; then
   echo "release does not exist"
@@ -27,5 +27,5 @@ if [ -z "$RELEASE_URL" ]; then
     -d "{\"tag_name\": \"$VERSION_TAG\", \"name\": \"$VERSION_TAG\", \"body\": \"Release $VERSION_TAG\" }"\
     -H "Content-Type: application/json"\
     -X POST \
-    https://api.github.com/repos/markdboyd/cumulus-message-adapter-js/releases
+    https://api.github.com/repos/nasa/cumulus-message-adapter-js/releases
 fi
