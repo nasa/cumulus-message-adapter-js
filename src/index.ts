e89700f1c0ed56677125f19840193d0bb7f08712
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-07-21 15:32:39 -0600

    Stop using module.exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import * as cma from './cma';
-
-module.exports = {
-  generateCMASpawnArguments: cma.generateCMASpawnArguments,
-  invokeCumulusMessageAdapter: cma.invokeCumulusMessageAdapter,
-  runCumulusTask: cma.runCumulusTask
-};
+export {
+  generateCMASpawnArguments,
+  invokeCumulusMessageAdapter,
+  runCumulusTask
+} from './cma';
